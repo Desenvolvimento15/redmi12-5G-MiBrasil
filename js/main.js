16b7c8fce0c1e0984a5496f050554a6e28e78fa3
Your Name <96912707+Desenvolvimento15@users.noreply.github.com>
2023-09-22 14:39:38 -0300

    vs01

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.galeria').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+ 
+  });
